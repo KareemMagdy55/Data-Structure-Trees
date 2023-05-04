c5b4c76a1a1f8c35c5e71ced73cff77554f87c76
Kareem Magdy <91559979+KareemMagdy55@users.noreply.github.com>
2023-05-04 17:26:40 +0300

    Add students database's cpp file .
    - Add students database's cpp file which its main function is to load all student information from the txt file "Stud_db.txt" into a container "students" that can be accessed from another file using the keyword "extern".
    
    - Test students database's cpp file in the main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "students_DB.cpp"
 using namespace std;
 
 int main() {
+    
+    loadTxtFile();
+    cout << students[0];
+
     return 0;
 }
---
+++ students_DB.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include "student.h"
+using namespace std;
+
+
+// extern denotes a global variable that can be accessed from a different cpp file.
+extern student* students = NULL; // must be initialized.
+extern int nStudents = 0 ; // must be initialized.
+
+
+extern void loadTxtFile (){
+    // open Stud_db.txt.
+    fstream students_DB ;
+    students_DB.open("Stud_db.txt");
+
+    // Get the number of students in the first line of the txt file.
+    string nStud ;
+    getline(students_DB, nStud);
+    nStudents = stoi(nStud);
+
+    //Initialize the student array "students" with the new size.
+    students = new student[nStudents];
+
+    // Load the desired information from the txt file into the students array.
+    for (int i = 0; i < nStudents; ++i) {
+        getline(students_DB, students[i].id);
+        getline(students_DB, students[i].name);
+        getline(students_DB, students[i].gpa);
+        getline(students_DB, students[i].dep);
+    }
+
+
+}
