4b461a0cf6e422c40bc37573d78f0c47fbe19790
Sara_11 <92929722+sarawalid99@users.noreply.github.com>
2023-05-09 00:51:18 +0300

    Avl done (adding Avl.h)

---
+++ cmake-build-debug/CMakeFiles/maxHeap.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for maxHeap.
+# This may be replaced when dependencies are built.
---
+++ cmake-build-debug/CMakeFiles/students_database.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/students_database.dir/Students_database/students_DB.cpp.obj"
+  "CMakeFiles/students_database.dir/Students_database/students_DB.cpp.obj.d"
+  "libstudents_database.dll.a"
+  "students_database.exe"
+  "students_database.exe.manifest"
+  "students_database.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/students_database.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
