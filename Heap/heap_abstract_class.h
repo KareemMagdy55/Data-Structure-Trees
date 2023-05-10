8095c72bd586a5647294e39e4836b8446c562cf4
Kareem Magdy <91559979+KareemMagdy55@users.noreply.github.com>
2023-05-10 04:29:15 +0300

    Add heap abstract class.

--- Heap/heap_abstract_class.h
+++ Heap/heap_abstract_class.h
@@ -1,6 +1,27 @@
 #pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
 
 template<typename T>
-class heap{
-    
+class Heap{
+protected:
+    vector<T> heap;
+    int size ;
+    virtual void eraseRoot() = 0 ;
+
+public:
+
+    virtual void insert(T element) = 0;
+    virtual void heapify(int index) = 0 ;
+    virtual void heapSort() = 0 ;
+    void constructHeap() {
+        for (int i = size / 2 - 1; i >= 0; --i)
+            heapify(i);
+    }
+    void printHeap(){
+        for(int i = 0 ; i < size ; i ++)
+            cout << heap[i] << ' ';
+        cout << '\n';
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include "Students_database/students_DB.cpp"
 using namespace std;
 
 int main() {
-    loadTxtFile();
-    cin >> students[0];
-    cout << students[0];
     return 0;
 }
