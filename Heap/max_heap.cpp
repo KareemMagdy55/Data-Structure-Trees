330bb347f4da2b2c5bbe70982cb7ab7000c8c3f1
Kareem Magdy <91559979+KareemMagdy55@users.noreply.github.com>
2023-05-10 04:29:55 +0300

    Add max heap's header and cpp files.

--- Heap/max_heap.cpp
+++ Heap/max_heap.cpp
@@ -1,2 +1,65 @@
 #pragma once
 #include "max_heap.h"
+
+MaxHeap::MaxHeap() {
+    loadTxtFile();
+    size = nStudents;
+
+    for (int i = 0; i < size; ++i) {
+       heap.push_back(students[i]);
+    }
+    constructHeap();
+}
+
+void MaxHeap::insert(student stud){
+    // insert new student at last node (index)
+    heap.push_back(stud);
+    size ++ ;
+    int idx = size -1 ;
+    int studIdx = idx ;
+
+    // compare parents to new node that was inserted
+    while (idx > 0){
+        if (heap[studIdx].gpa > heap[idx].gpa) {// if child > parent , then swap them.
+            swap(heap[studIdx], heap[idx]);
+            studIdx = idx ;
+        }
+        idx /= 2;
+    }
+    if (heap[studIdx].gpa > heap[0].gpa){
+        swap ( heap[studIdx], heap[0]);
+    }
+
+}
+void MaxHeap::heapify(int index){
+    int max = index;
+
+    int left  = 2 * index + 1 ;
+    int right = left + 1 ;
+
+    if (heap[left].gpa > heap[max].gpa and left < size)
+        max = left ;
+
+    if ( heap[right].gpa > heap[max].gpa and right < size)
+        max = right;
+
+    if ( max != index){
+        swap ( heap[max] , heap[index]);
+        heapify(max);
+    }
+}
+void MaxHeap::eraseRoot() {
+    swap(heap[0] , heap[size - 1]);
+    size -- ;
+    heapify(0);
+}
+void MaxHeap::heapSort() {
+    int sz = size;
+    constructHeap();
+
+    while (size > 0) {
+        eraseRoot();
+    }
+    size = sz;
+}
+
--- Heap/max_heap.h
+++ Heap/max_heap.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "heap_abstract_class.h"
+#include "students_DB.cpp"
+
+class MaxHeap : public Heap<student>{
+public:
+    void insert(student stud) override;
+    void heapify(int index) override;
+    void heapSort() override;
+    void eraseRoot() override;
+    void seeGpa(){
+        for (int i = 0; i < size; ++i) {
+            cout << heap[i].gpa << ", ";
+        }
+        cout << endl;
+    }
+    MaxHeap();
+};
